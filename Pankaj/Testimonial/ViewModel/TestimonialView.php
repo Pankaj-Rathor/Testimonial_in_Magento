36c3bc937c5750af6df4a49368627a1c04d626ab
Pankaj Rathor <prathore9669@gmail.com>
2023-10-25 17:28:44 +0530

    25-10-2023

--- Pankaj/Testimonial/ViewModel/TestimonialView.php
+++ Pankaj/Testimonial/ViewModel/TestimonialView.php
@@ -6,20 +6,28 @@ use Magento\Framework\UrlInterface;
 class TestimonialView implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     protected $urlInterface;
+    protected $assetRepo;
 
     public function __construct(
-        UrlInterface $urlInterface
+        UrlInterface $urlInterface,
+        \Magento\Framework\View\Asset\Repository $assetRepo
     )
     {
         $this->urlInterface = $urlInterface;
+        $this->_assetRepo = $assetRepo;
     }
 
     public function getImageUrl($image)
     {
-        if (!empty($image)) {
-            return $this->urlInterface->getBaseUrl(['_type' => UrlInterface::URL_TYPE_MEDIA]).'testimonial/image/'.$image;            
+        if (!empty($image))
+        {
+            return $this->urlInterface->getBaseUrl(['_type' => UrlInterface::URL_TYPE_MEDIA]).'testimonial/image/'.$image;
         }
-        //Default Image
-        return $this->urlInterface->getBaseUrl(['_type' => UrlInterface::URL_TYPE_MEDIA]).'testimonial/image/'.$image;
+        return $this->getPlaceholderImage();
+    }
+
+    public function getPlaceholderImage()
+    {
+       return $this->_assetRepo->getUrl("Pankaj_Testimonial::images/default_user.png");
     }
 }
